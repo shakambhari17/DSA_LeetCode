14b7f204d24028c3b3a3f2282605fa907d3264ad
shakambhari17 <srivastavajyoti43711@gmail.com>
2026-03-23 23:39:22 +0530

    Time: 0 ms (100%), Space: 12.4 MB (43.24%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        
+        i=0
+        while (i < len(nums)):
+            if nums[i]==val:
+                nums[i],nums[-1]=nums[-1],nums[i]
+                nums.pop()
+            else:
+                i+=1
+        return len(nums)
