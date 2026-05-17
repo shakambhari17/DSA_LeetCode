f96feb2d6d5a6e11783c991f45990da526578e18
shakambhari17 <srivastavajyoti43711@gmail.com>
2026-05-17 19:14:38 +0530

    Time: 13 ms (91.69%), Space: 12.5 MB (67.4%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        freq1={}
+        freq2={}
+
+        for ch in s:
+            if ch in freq1:
+                freq1[ch] +=1
+            else:
+                freq1[ch]= 1
+        for ch in t:
+            if ch in freq2:
+                freq2[ch] +=1
+            else:
+                freq2[ch] =1
+        return freq1 == freq2
