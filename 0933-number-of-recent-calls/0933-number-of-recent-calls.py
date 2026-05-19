d37c6b8c330cad80091b4a5db9d20306d7a3c014
shakambhari17 <srivastavajyoti43711@gmail.com>
2026-05-19 11:42:02 +0530

    Time: 76 ms (36.81%), Space: 17.4 MB (24.31%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,10 @@
+class RecentCounter(object):
+
+    def __init__(self):
+        self.queue = deque()
+    def ping(self, t):
+        self.queue.append(t)
+
+        while self.queue[0] < t- 3000:
+            self.queue.popleft()
+        return len(self.queue)
