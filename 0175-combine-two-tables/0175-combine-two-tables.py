b01f2ab4b0aa817576c7f79e7539401d43815ea0
shakambhari17 <srivastavajyoti43711@gmail.com>
2026-06-19 23:15:28 +0530

    Time: 268 ms (77.14%), Space: 69 MB (8.27%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    
+    merged_df = pd.merge(person, address, on='personId', how='left')
+    
+    
+    result_df = merged_df[['firstName', 'lastName', 'city', 'state']]
+    
+    return result_df
